371cce07b7f1b813b27fb0ba31ce13af25d517ad
Ivan Kichigin <ivan.kichigin977@gamil.com>
2023-12-25 23:09:55 +0800

    createControllers

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,62 +3,66 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use Carbon\Carbon;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+  public function createForm()
+  {
+      return view('posts.create');
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  public function show(Post $post)
+  {
+      if ($post->isPublished() || auth()->user() && auth()->user()->id === $post->user->id){
+          return view('posts.show', compact('post'));
+      }
+      return abort('404');
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  public function create(Request $request)
+{
+  $request->validate([
+      'title' => 'required|max:255',
+      'content' => 'required',
+  ]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
+  $publishedAt = $request->has('schedule_post') ? $request->input('published_at') : null;
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
+  auth()->user()->posts()->create([
+      'title' => $request->input('title'),
+      'content' => $request->input('content'),
+      'published_at' => $publishedAt,
+  ]);
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+  return redirect()->route('home')->with('success', 'Пост успешно создан');
+}
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+  public function edit(Post $post)
+  {
+      return view('posts.edit', compact('post'));
+  }
+
+  public function update(Request $request, Post $post)
+  {
+      $request->validate([
+          'title' => 'required|string|max:255',
+          'content' => 'required|string',
+      ]);
+
+      $post->update([
+          'title' => $request->title,
+          'content' => $request->content,
+      ]);
+
+      return redirect()->route('home')->with('status', 'Пост успешно обновлен.');
+  }
+
+  public function destroy(Post $post)
+  {
+      $post->delete();
+
+      return redirect()->route('home')->with('status', 'Пост успешно удален.');
+  }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Carbon\Carbon; //Для времени
 class Post extends Model
 {
-    use HasFactory;
+  protected $fillable = ['user_id', 'title', 'content', 'published_at'];
+  protected $dates = ['published_at'];
+
+  public function user()
+  {
+      return $this->belongsTo(User::class);
+  }
+
+  public function comments()
+  {
+      return $this->hasMany(Comment::class);
+  }
+
+  public function isPublished()
+  {
+      return $this->published_at === null || $this->published_at <= Carbon::now();
+  }
+
+  public function isSoonToBePublished()
+  {
+      return $this->published_at !== null && $this->published_at > Carbon::now() && $this->published_at <= Carbon::now()->addHours(1);
+  }
 }
